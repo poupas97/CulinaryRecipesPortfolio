888f015f0ca8f9b61ba2d3dc497f912794255dd4
Ruben Carreira <ruben_carreira97@hotmail.com>
2020-11-02 22:27:55 +0000

    manage recipe: initial apis

---
+++ server/api/controllers/RecipeController.js
@@ -0,0 +1,62 @@
+const RecipeConnection = require('../../connections/RecipeConnection');
+const { ErrorMapper, errorDtoSimple } = require('../../dto/ErrorDTO');
+
+const listRecipes = async (req, res) => {
+  try {
+    const result = await RecipeConnection.listRecipes();
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(errorDtoSimple(error));
+  }
+};
+
+const singleRecipeById = async (req, res) => {
+  try {
+    const { params: { id } } = req;
+
+    if (!id) return res.status(500).json(errorDtoSimple(ErrorMapper.MISS_ID));
+
+    const result = await RecipeConnection.singleRecipeById(id);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(errorDtoSimple(error));
+  }
+};
+
+const createRecipe = async (req, res) => {
+  try {
+    const { body: { name, description } } = req;
+    const result = await RecipeConnection.createRecipe({ name, description });
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(errorDtoSimple(error));
+  }
+};
+
+const updateRecipe = async (req, res) => {
+  try {
+    const { body: { name, description, active }, params: { id } } = req;
+
+    if (!id) return res.status(500).json(errorDtoSimple(ErrorMapper.MISS_ID));
+
+    const result = await RecipeConnection.updateRecipe({ name, description, active }, id);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(errorDtoSimple(error));
+  }
+};
+
+const deleteRecipe = async (req, res) => {
+  try {
+    const { params: { id } } = req;
+
+    if (!id) return res.status(500).json(errorDtoSimple(ErrorMapper.MISS_ID));
+
+    const result = await RecipeConnection.deleteRecipe(id);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(errorDtoSimple(error));
+  }
+};
+
+module.exports = () => ({ listRecipes, singleRecipeById, createRecipe, updateRecipe, deleteRecipe });
